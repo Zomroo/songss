0f3cf70ce6dc6b92a8488900e8d5b3ed80a958f5
Zomroo <93994060+Zomroo@users.noreply.github.com>
2023-03-24 04:24:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 # create a new Pyrogram client instance
-api_id = os.environ.get("16844842")
-api_hash = os.environ.get("f6b0ceec5535804be7a56ac71d08a5d4")
-bot_token = os.environ.get("6145559264:AAFufTIozcyIRZPf9bRWCvky2_NhbbjWTKU")
+api_id = 16844842
+api_hash = "f6b0ceec5535804be7a56ac71d08a5d4"
+bot_token = "6145559264:AAFufTIozcyIRZPf9bRWCvky2_NhbbjWTKU"
 app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
 # define a function to search for songs on YouTube
