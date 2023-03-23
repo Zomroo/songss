0bdae6e990c5966df0a0df7c66603c470594351a
Zomroo <93994060+Zomroo@users.noreply.github.com>
2023-03-24 04:28:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,28 +2,27 @@ import os
 import requests
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-import requests
-import re
+import re	
 
 
-# create a new Pyrogram client instance
-api_id = 16844842
-api_hash = "f6b0ceec5535804be7a56ac71d08a5d4"
+
+# create a new Pyrogram client instance	# create a new Pyrogram client instance
+api_id = 16844842	api_id = os.environ.get("API_ID")
+api_hash = "f6b0ceec5535804be7a56ac71d08a5d4"	api_hash = os.environ.get("API_HASH")
 bot_token = "6145559264:AAFufTIozcyIRZPf9bRWCvky2_NhbbjWTKU"
 app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
-
 # define a function to search for songs on YouTube
 def search_youtube(query):
     url = "https://www.youtube.com/results"
     params = {"search_query": query}
     response = requests.get(url, params=params)
-    search_results = re.findall(r'/watch\?v=(.{11})', response.text)
-    video_ids = []
-    for search_result in search_results:
-        if len(video_ids) == 5:
-            break
-        video_ids.append(search_result)
-    return video_ids
+    search_results = re.findall(r'/watch\?v=(.{11})" title="(.*?)"', response.text)
+    results = []
+    for search_result in search_results[:5]:
+        video_id = search_result[0]
+        title = search_result[1]
+        results.append({"title": title, "video_id": video_id})
+    return results
 
 # define a command handler for the /start command
 @app.on_message(filters.command("start"))
@@ -38,17 +37,18 @@ def song_command(client, message):
     query = message.text.replace("/song", "").strip()
     
     # search for the query on YouTube
-    video_ids = search_youtube(query)
+    results = search_youtube(query)
     
     # create a list of InlineKeyboardButton objects for the search results
     buttons = []
-    for i, video_id in enumerate(video_ids):
-        button = InlineKeyboardButton(str(i+1), callback_data=video_id)
+    for i, result in enumerate(results):
+        button_label = f"{i+1}. {result['title']}"
+        button = InlineKeyboardButton(button_label, callback_data=result["video_id"])
         buttons.append(button)
     keyboard = InlineKeyboardMarkup([buttons])
     
     # send a message with the search results as buttons
-    message_text = "Here are 5 results for '{}':".format(query)
+    message_text = f"Here are 5 results for '{query}':"
     client.send_message(chat_id=message.chat.id, text=message_text, reply_markup=keyboard)
 
 # define a callback query handler for the search result buttons
@@ -58,7 +58,7 @@ def callback_query(client, callback_query):
     video_id = callback_query.data
     
     # send the audio of the selected video to the user
-    url = "https://www.youtube.com/watch?v={}".format(video_id)
+    url = f"https://www.youtube.com/watch?v={video_id}"
     video = pafy.new(url)
     bestaudio = video.getbestaudio()
     audio_url = bestaudio.url
